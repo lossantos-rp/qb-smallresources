6e2182a519ef79339672f6a2a65f68a4220e403a
IamMikejuuh <mikehetapestaartje@gmail.com>
2022-06-07 20:16:21 +0200

    discord rpc

--- client/discord.lua
+++ client/discord.lua
@@ -3,24 +3,24 @@
 CreateThread(function()
     while true do
         -- This is the Application ID (Replace this with you own)
-	SetDiscordAppId()
+	SetDiscordAppId(903002520887123989)
 
         -- Here you will have to put the image name for the "large" icon.
-	SetDiscordRichPresenceAsset('logo_name')
+	SetDiscordRichPresenceAsset('lsrp-full')
         
         -- (11-11-2018) New Natives:
 
         -- Here you can add hover text for the "large" icon.
-        SetDiscordRichPresenceAssetText('This is a lage icon with text')
+        SetDiscordRichPresenceAssetText('🔵 Project Los Santos RolePlay 🔵')
        
         -- Here you will have to put the image name for the "small" icon.
-        SetDiscordRichPresenceAssetSmall('logo_name')
+        SetDiscordRichPresenceAssetSmall('wip')
 
         -- Here you can add hover text for the "small" icon.
-        SetDiscordRichPresenceAssetSmallText('This is a lsmall icon with text')
+        SetDiscordRichPresenceAssetSmallText('🚧 IN DEVELOPMENT 🚧')
 
         QBCore.Functions.TriggerCallback('smallresources:server:GetCurrentPlayers', function(result)
-            SetRichPresence('Players: '..result..'/64')
+            SetRichPresence('Players: '..result..'/8')
         end)
 
         -- (26-02-2021) New Native:
@@ -30,8 +30,8 @@ CreateThread(function()
             First paramater is the button index (0 or 1), second is the title and 
             last is the url (this has to start with "fivem://connect/" or "https://") 
         ]]--
-        SetDiscordRichPresenceAction(0, "First Button!", "fivem://connect/localhost:30120")
-        SetDiscordRichPresenceAction(1, "Second Button!", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(0, "Discord (SOON)", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(1, "Website (SOON)", "fivem://connect/localhost:30120")
 
         -- It updates every minute just in case.
 	Wait(60000)
